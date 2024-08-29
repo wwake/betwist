b7d22a4bb6d7352c53e8bd6822e8229f1c21982d
Bill Wake <william.wake@acm.org>
2024-08-29 19:42:59 -0400

    Initial xcode checkin

---
+++ betwist/betwist/betwistApp.swift
@@ -0,0 +1,17 @@
+//
+//  betwistApp.swift
+//  betwist
+//
+//  Created by Bill Wake on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct betwistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
