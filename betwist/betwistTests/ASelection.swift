ccd50761d34a6b8dbda768b81cc05c8317065871
Bill Wake <william.wake@acm.org>
2024-09-24 16:35:50 -0400

    Creater initial selection object (not complete)

---
+++ betwist/betwistTests/ASelection.swift
@@ -0,0 +1,8 @@
+@testable import betwist
+import Testing
+
+struct ASelection {
+    @Test("test zero") func zero() async throws {
+        Issue.record("Tests not yet implemented in ASelection")
+    }
+}
