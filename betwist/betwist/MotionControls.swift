2ba9675c20f7b7a217c21355d4947674025a61d9
Bill Wake <william.wake@acm.org>
2024-10-01 14:47:18 -0400

    Extract helper method button(Directions: String:)

--- betwist/betwist/MotionControls.swift
+++ betwist/betwist/MotionControls.swift
@@ -3,54 +3,29 @@ import SwiftUI
 struct MotionControls: View {
   @Binding var game: Game
 
+  func button(_ direction: Directions, _ imageName: String) -> some View {
+    Button {
+      withAnimation {
+        game.twist(direction)
+      }
+    } label: {
+      Image(systemName: imageName)
+        .imageScale(.large)
+    }
+    .tint(.white)
+    .buttonStyle(.bordered)
+  }
+
   var body: some View {
     VStack {
-      Button {
-        withAnimation {
-          game.twist(.up)
-        }
-      } label: {
-        Image(systemName: "arrow.up.circle")
-          .imageScale(.large)
-      }
-      .buttonStyle(.bordered)
-      .tint(.mint)
+      button(.up, "arrow.up.circle")
 
       HStack {
-        Button {
-          withAnimation {
-            game.twist(.left)
-          }
-        } label: {
-          Image(systemName: "arrow.left.circle")
-            .imageScale(.large)
-        }
-        .buttonStyle(.bordered)
-        .tint(.mint)
-
-        Button {
-          withAnimation {
-            game.twist(.right)
-          }
-        } label: {
-          Image(systemName: "arrow.right.circle")
-            .imageScale(.large)
-        }
+        button(.left, "arrow.left.circle")
+        button(.right, "arrow.right.circle")
       }
-      .buttonStyle(.bordered)
-      .tint(.mint)
 
-      Button {
-        withAnimation {
-          game.twist(.down)
-        }
-      } label: {
-        Image(systemName: "arrow.down.circle")
-          .imageScale(.large)
-      }
-      .buttonStyle(.bordered)
-      .tint(.mint)
-
+      button(.down, "arrow.down.circle")
     }
   }
 }
