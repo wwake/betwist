71a9c876eed8495ddea0e4de4b3deb31c88a3dc2
Bill Wake <william.wake@acm.org>
2024-10-01 15:11:02 -0400

    Extract Twister (and Directions) to a new file

--- betwist/betwist/Grid.swift
+++ betwist/betwist/Grid.swift
@@ -1,45 +1,6 @@
 import Algorithms
 import Foundation
 
-enum Directions {
-  case up, down, left, right
-}
-
-struct Twister {
-  var size: Int
-
-  var corner: Location
-
-  init(_ size: Int) {
-    self.size = size
-    self.corner = Location(0, 0)
-  }
-
-  mutating func twist(_ direction: Directions) {
-    switch direction {
-    case .left:
-      corner = Location(corner.row, (corner.column + 1) %% size)
-
-    case .right:
-      corner = Location(corner.row, (corner.column - 1) %% size)
-
-    case .up:
-      corner = Location((corner.row + 1) %% size, corner.column)
-
-    case .down:
-      corner = Location((corner.row - 1) %% size, corner.column)
-    }
-  }
-
-  var rowIndexes: [Int] {
-    (0..<size).map { (corner.row + $0) %% size }
-  }
-
-  var columnIndexes: [Int] {
-    (0..<size).map { (corner.column + $0) %% size }
-  }
-}
-
 struct Grid {
   let size: Int
   let grid: [[String]]
