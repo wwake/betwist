b6f003e62ccfe2eb0ea911c65ad866406a7163b1
Bill Wake <william.wake@acm.org>
2024-10-14 16:12:35 -0400

    Refactor - extract functions for subviews

--- betwist/betwist/ContentView.swift
+++ betwist/betwist/ContentView.swift
@@ -5,6 +5,94 @@ struct ContentView: View {
   @State private var guessOffset: CGFloat = 0
   @State private var showGuesses = false
 
+  func guessView() -> some View {
+    HStack {
+      Image("TheIcon")
+        .resizable()
+        .scaledToFit()
+        .frame(maxWidth: 128, maxHeight: 128)
+        .accessibilityLabel(Text("Betwist"))
+      Text(game.guess)
+        .font(.largeTitle)
+        .foregroundStyle(game.message.isEmpty ? Color.black : Color.red)
+        .frame(minWidth: 230)
+        .frame(height: 40)
+        .padding(4)
+        .background(Color(white: 1.0, opacity: 0.85))
+        .border(.black, width: 2)
+        .accessibilityAddTraits(.isButton)
+        .offset(y: guessOffset)
+        .onTapGesture {
+          collectWord()
+        }
+      Button {
+        collectWord()
+      } label: {
+        Image(systemName: "checkmark.circle.fill")
+          .accessibilityLabel(Text("Enter"))
+          .font(.largeTitle)
+          .foregroundStyle(.green)
+      }
+    }
+    .zIndex(1)
+  }
+
+  func gridView() -> some View {
+    VStack(spacing: 0) {
+      ForEach(game.rowIndexes, id: \.self) { row in
+        HStack(spacing: 0) {
+          ForEach(game.columnIndexes, id: \.self) { column in
+            Button {
+              let location = Location(row, column)
+              if game.lastLocationSelected(was: location) {
+                collectWord()
+              } else {
+                game.select(location)
+              }
+            } label: {
+              Text("\(game[row, column])")
+            }
+            .modifier(GridButtonStyle(game, Location(row, column)))
+          }
+        }
+      }.gesture(
+        DragGesture(minimumDistance: 25)
+// Spike live scrolling
+//              .onChanged { dragInfo in
+//                print("Start: \(dragInfo.startLocation.x) \(dragInfo.startLocation.y)")
+//                print("Move horizontally \(Int(dragInfo.location.x - dragInfo.startLocation.x) / 50)")
+//                print("Move vertically \(Int(dragInfo.location.y - dragInfo.startLocation.y)/50)")
+//              }
+          .onEnded { dragInfo in
+            withAnimation {
+              game.twist(dragInfo.startLocation.direction(to: dragInfo.location))
+            }
+          }
+      )
+    }
+  }
+
+  func answersView() -> some View {
+    HStack {
+      Button {
+        showGuesses.toggle()
+      } label: {
+        Image(systemName: "doc.text.magnifyingglass")
+          .accessibilityLabel(Text("View Guesses"))
+      }
+      .opacity(game.guesses.isEmpty ? 0 : 1)
+
+      ScrollView {
+        Text(verbatim: game.guesses.description)
+          .font(.title2)
+          .frame(width: 300)
+        Divider()
+//            Text("All answers")
+//            Text(verbatim: "\(game.allAnswers)")
+      }
+    }
+  }
+
   fileprivate func collectWord() {
     if game.guess.isEmpty { return }
 
@@ -25,91 +113,15 @@ struct ContentView: View {
   var body: some View {
     ZStack {
       VStack {
-        HStack {
-          Image("TheIcon")
-            .resizable()
-            .scaledToFit()
-            .frame(maxWidth: 128, maxHeight: 128)
-            .accessibilityLabel(Text("Betwist"))
-          Text(game.guess)
-            .font(.largeTitle)
-            .foregroundStyle(game.message.isEmpty ? Color.black : Color.red)
-            .frame(minWidth: 230)
-            .frame(height: 40)
-            .padding(4)
-            .background(Color(white: 1.0, opacity: 0.85))
-            .border(.black, width: 2)
-            .accessibilityAddTraits(.isButton)
-            .offset(y: guessOffset)
-            .onTapGesture {
-              collectWord()
-            }
-          Button {
-            collectWord()
-          } label: {
-            Image(systemName: "checkmark.circle.fill")
-              .accessibilityLabel(Text("Enter"))
-              .font(.largeTitle)
-              .foregroundStyle(.green)
-          }
-        }
-        .zIndex(1)
+        guessView()
 
         Text(game.message)
           .foregroundStyle(.red)
           .frame(height: 20)
 
-        VStack(spacing: 0) {
-          ForEach(game.rowIndexes, id: \.self) { row in
-            HStack(spacing: 0) {
-              ForEach(game.columnIndexes, id: \.self) { column in
-                Button {
-                  let location = Location(row, column)
-                  if game.lastLocationSelected(was: location) {
-                    collectWord()
-                  } else {
-                    game.select(location)
-                  }
-                } label: {
-                  Text("\(game[row, column])")
-                }
-                .modifier(GridButtonStyle(game, Location(row, column)))
-              }
-            }
-          }.gesture(
-            DragGesture(minimumDistance: 25)
-// Spike live scrolling
-//              .onChanged { dragInfo in
-//                print("Start: \(dragInfo.startLocation.x) \(dragInfo.startLocation.y)")
-//                print("Move horizontally \(Int(dragInfo.location.x - dragInfo.startLocation.x) / 50)")
-//                print("Move vertically \(Int(dragInfo.location.y - dragInfo.startLocation.y)/50)")
-//              }
-              .onEnded { dragInfo in
-                withAnimation {
-                  game.twist(dragInfo.startLocation.direction(to: dragInfo.location))
-                }
-              }
-          )
-        }
+        gridView()
 
-        HStack {
-          Button {
-            showGuesses.toggle()
-          } label: {
-            Image(systemName: "doc.text.magnifyingglass")
-              .accessibilityLabel(Text("View Guesses"))
-          }
-          .opacity(game.guesses.isEmpty ? 0 : 1)
-
-          ScrollView {
-            Text(verbatim: game.guesses.description)
-              .font(.title2)
-              .frame(width: 300)
-            Divider()
-//            Text("All answers")
-//            Text(verbatim: "\(game.allAnswers)")
-          }
-        }
+        answersView()
 
         Spacer()
       }
