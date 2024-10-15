7e67d6b6e906ccda1df81498f22544eeae4e6b8a
Bill Wake <william.wake@acm.org>
2024-10-15 14:32:00 -0400

    Replace Button for letters with a shape

--- betwist/betwist/LetterView.swift
+++ betwist/betwist/LetterView.swift
@@ -9,21 +9,34 @@ struct LetterView: View {
   var chooser = ColorChooser()
 
   var body: some View {
-//    Rectangle()
-//      .strokeBorder(chooser.borderColor(game.type(at: Location(row, column))), lineWidth: 2)
-//      .fill(chooser.backgroundColor(game.type(at: Location(row, column)), hue: game.hue(at: Location(row, column))))
-//      .foregroundStyle(chooser.foregroundColor(game.type(at: Location(row, column))))
-//      .frame(width: 50, height: 50)
-
-    Button {
-      if game.lastLocationSelected(was: location) {
-        collectWord()
-      } else {
-        game.select(location)
+    Rectangle()
+      .stroke(chooser.borderColor(game.type(at: location)), lineWidth: 4.0)
+      .fill(chooser.backgroundColor(game.type(at: location), hue: game.hue(at: location)))
+      .frame(width: 50, height: 50)
+      .overlay {
+        Text("\(game[location])")
+          .font(.largeTitle)
+          .italic(game.type(at: location) == .neighbor)
+          .foregroundStyle(chooser.foregroundColor(game.type(at: location)))
+          .allowsHitTesting(false)
       }
-    } label: {
-      Text("\(game[location])")
-    }
-    .modifier(GridButtonStyle(game, location))
+      .onTapGesture {
+        if game.lastLocationSelected(was: location) {
+          collectWord()
+        } else {
+          game.select(location)
+        }
+      }
+
+//    Button {
+//      if game.lastLocationSelected(was: location) {
+//        collectWord()
+//      } else {
+//        game.select(location)
+//      }
+//    } label: {
+//      Text("\(game[location])")
+//    }
+//    .modifier(GridButtonStyle(game, location))
   }
 }
