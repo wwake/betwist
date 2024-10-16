a2106c9543195520d190b005986c254fb120bd0c
Bill Wake <william.wake@acm.org>
2024-10-16 15:18:12 -0400

    Refactor - extract method to wrapDouble()

--- betwist/betwist/CGSize+wrap.swift
+++ betwist/betwist/CGSize+wrap.swift
@@ -1,25 +1,26 @@
 import Foundation
 
 extension CGSize {
+  fileprivate func wrapDouble(_ input: Double, wrap: Double) -> Double {
+    var value = input
+    let boundary = 0.5 * wrap
+
+    while value < -boundary {
+      value += wrap
+    }
+
+    while value >= boundary {
+      value -= wrap
+    }
+
+    return value
+  }
+
   func wrap(_ wrap: Double) -> CGSize {
     var offset = self
 
-    let boundary = 0.5 * wrap
-    while offset.width < -boundary {
-      offset.width += wrap
-    }
-
-    while offset.width >= boundary {
-      offset.width -= wrap
-    }
-
-    while offset.height < -boundary {
-      offset.height += wrap
-    }
-
-    while offset.height >= boundary {
-      offset.height -= wrap
-    }
+    offset.width = wrapDouble(offset.width, wrap: wrap)
+    offset.height = wrapDouble(offset.height, wrap: wrap)
 
     return offset
   }
