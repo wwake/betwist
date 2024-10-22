41c9a4402c8ea01743b4aa892ee768ba0e34427c
Bill Wake <william.wake@acm.org>
2024-10-22 13:50:56 -0400

    todo, keynote

--- betwist/betwist/model/Twister.swift
+++ betwist/betwist/model/Twister.swift
@@ -31,3 +31,13 @@ struct Twister {
     (0..<size).map { (corner.column + $0) %% size }
   }
 }
+
+// 0,0  0,1  0,2   ABC
+// 1,0  1,1  1,2   DEF
+// 2,0  2,1  2,2   GHI
+
+//=>
+
+// 0,2  1,2  2,2   CFI
+// 0,1  1,1  2,1   BEH
+// 0,0  1,0  2,0   ADG
