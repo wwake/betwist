5e5940c80c021ca0441f536c2bba9f14ea959cbe
Bill Wake <william.wake@acm.org>
2024-11-04 15:05:40 -0500

    Make Trie contains() use helper that knows character indexes

--- betwist/betwist/model/Trie.swift
+++ betwist/betwist/model/Trie.swift
@@ -5,11 +5,16 @@ struct Trie {
   let next: [(Character, Trie)]
 
   func contains(_ value: String) -> Bool {
-    if value.isEmpty {
+    let stringAsArray = Array(value)
+    return contains(stringAsArray, 0)
+  }
+
+  func contains(_ value: [Character], _ index: Int) -> Bool {
+    if index == value.count {
       return isWord
     }
-    let first = value.first!
+    let first = value[index]
     let node = next.first(where: { $0.0 == first })
-    return node?.1.contains(String(value.dropFirst())) ?? false
+    return node?.1.contains(value, index + 1) ?? false
   }
 }
