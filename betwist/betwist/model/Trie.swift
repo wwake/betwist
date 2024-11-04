339220981224ae7873c62279a37b3bc6b4b13e9a
Bill Wake <william.wake@acm.org>
2024-11-04 15:13:50 -0500

    Removed recursion from Trie.contains()

--- betwist/betwist/model/Trie.swift
+++ betwist/betwist/model/Trie.swift
@@ -5,16 +5,14 @@ struct Trie {
   let next: [(Character, Trie)]
 
   func contains(_ value: String) -> Bool {
-    let stringAsArray = Array(value)
-    return contains(stringAsArray, 0)
-  }
+    let target = Array(value)
 
-  func contains(_ value: [Character], _ index: Int) -> Bool {
-    if index == value.count {
-      return isWord
+    var trie = self
+    for ch in target {
+      let node = next.first(where: { $0.0 == ch })
+      if node == nil { return false }
+      trie = node!.1
     }
-    let first = value[index]
-    let node = next.first(where: { $0.0 == first })
-    return node?.1.contains(value, index + 1) ?? false
+    return trie.isWord
   }
 }
