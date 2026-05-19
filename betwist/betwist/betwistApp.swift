4452176acc7911721c8133886e28142298b37458
Bill Wake <william.wake@acm.org>
2026-05-19 10:57:37 -0400

    Use Trie2 now

--- betwist/betwist/betwistApp.swift
+++ betwist/betwist/betwistApp.swift
@@ -5,10 +5,7 @@ struct betwistApp: App {
   @State private var game = Game(
     5,
     GameMaker(5).make(),
-    Vocabulary(
-      Trie.read("trie"),
-      Trie2.read("trie")
-    )
+    Vocabulary(Trie2.read("trie"))
   )
 
   var body: some Scene {
--- betwist/betwist/model/Vocabulary.swift
+++ betwist/betwist/model/Vocabulary.swift
@@ -6,11 +6,9 @@ struct SearchResult: Equatable {
 }
 
 class Vocabulary {
-  let trie: Trie
   let trie2: Trie2
 
-  init(_ trie: Trie, _ trie2: Trie2) {
-    self.trie = trie
+  init(_ trie2: Trie2) {
     self.trie2 = trie2
   }
 
@@ -19,18 +17,13 @@ class Vocabulary {
   }
 
   func containsAndPrefixes(_ target: String) -> SearchResult {
-    let result = trie.containsAndPrefixes(target)
-    let result2 = trie2.containsAndPrefixes(target)
-    if result != result2 {
-      print("Expected \(result) but got \(result2) for target \(target)")
-    }
-    return result
+    trie2.containsAndPrefixes(target)
   }
 }
 
 class NullVocabulary: Vocabulary {
   init() {
-    super.init(Trie([]), Trie2(data: TrieData(data: Data())))
+    super.init(Trie2(data: TrieData(data: Data())))
   }
 
   override func contains(_ word: String) -> Bool {
