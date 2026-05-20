5d7ad5a766f04458845eca137126199a6448f7dd
Bill Wake <william.wake@acm.org>
2026-05-20 09:58:59 -0400

    Removed dead comments, tweaked trie generation, removed obsolete json file

--- betwist/betwist/view/MakerView.swift
+++ betwist/betwist/view/MakerView.swift
@@ -10,14 +10,7 @@ struct MakerView: View {
         do {
           let words = WordLoader.load()
           let builder = TrieBuilder().add(words)
-//          let trie = builder.make()
-//
-//          let url = URL.documentsDirectory.appending(path: "trie.json-compressed")
-//          try trie.write(url)
-//          print("Copy \(url) to project's resources folder")
-
-          let dataBuilder = DataBuilder()
-          let data = dataBuilder.make(trie: builder.root)
+          let data = DataBuilder().make(trie: builder.root)
           let url2 = URL.documentsDirectory.appending(path: "trie.dat")
           try Trie2.write(url2, data)
           print("Copy \(url2) to project's resources folder")
