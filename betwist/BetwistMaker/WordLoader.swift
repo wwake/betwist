5ce76bfb6e679e764011c0be8cfeb538ad23c32a
Bill Wake <william.wake@acm.org>
2026-06-01 09:30:48 -0400

    Maker app can write new trie

--- betwist/BetwistMaker/WordLoader.swift
+++ betwist/BetwistMaker/WordLoader.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 enum WordLoader {
-  static func load(minimumSize: Int) -> [String] {
-    let path = Bundle.main.path(forResource: "words", ofType: "list")!
-    let words = try! String(contentsOfFile: path, encoding: .utf8)
-      .split(separator: "\n")
-      .map { String($0).uppercased() }
-      .filter { $0.count >= minimumSize }
-    return words
+  static func load(path: String, minimumSize: Int) -> [String] {
+    do {
+      let words = try String(contentsOfFile: path, encoding: .utf8)
+        .split(separator: "\n")
+        .map { String($0).uppercased() }
+        .filter { $0.count >= minimumSize }
+      return words
+    } catch {
+      print("WordLoader: Can't load words from \(path)")
+      return []
+    }
   }
 }
--- betwist/BetwistMaker/main.swift
+++ betwist/BetwistMaker/main.swift
@@ -1,13 +1,30 @@
 import Foundation
 
+// Instructions:
+// Copy words.list from the resources folder to the downloads directory
+// Run this script
+// Copy trie.dat from the downloads directory to the resources folder
+
 do {
-  let words = WordLoader.load(minimumSize: 4)
+  let folderURL = try FileManager.default.url(
+    for: .downloadsDirectory,
+    in: .userDomainMask,
+    appropriateFor: nil,
+    create: false
+  )
+
+  let fileURL = folderURL
+    .appendingPathComponent("words.list")
+
+  let words = WordLoader.load(path: fileURL.path(), minimumSize: 4)
+
   let builder = TrieBuilder().add(words)
+
   let dataBuilder = DataBuilder()
-  let _ = dataBuilder.make(trie: builder.root)
-  let url = URL.documentsDirectory.appending(path: "trie.dat")
+  _ = dataBuilder.make(trie: builder.root)
+  let url = URL.downloadsDirectory.appending(path: "trie.dat")
   try dataBuilder.write(url)
   print("Copy \(url) to project's resources folder")
 } catch {
-  print(error.localizedDescription)
+  print("main: Couldn't write trie due to \(error.localizedDescription)")
 }
