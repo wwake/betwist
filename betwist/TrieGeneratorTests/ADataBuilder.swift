585426184a90f5c1b4a4fd7da087b4042bb5ee0b
Bill Wake <william.wake@acm.org>
2026-06-11 10:55:47 -0400

    Move tests from BetwistMakerTests to TrieGeneratorTests

---
+++ betwist/TrieGeneratorTests/ADataBuilder.swift
@@ -0,0 +1,42 @@
+import Testing
+
+@testable import TrieGenerator
+
+struct ADataBuilder {
+  @Test
+  func convert_empty_word_list_to_buffer() {
+    let data = DataBuilder().make(trie: MakerTrie(next: []))
+    #expect(data[quadbyte: 0] == 0x00ffffff)
+  }
+
+  @Test
+  func convert_single_word_to_buffer() {
+    let trie = TrieBuilder().add(["A"]).root
+    let sut = DataBuilder()
+
+    let data = sut.make(trie: trie)
+
+    #expect(data[quadbyte: 0] == 0x00ffffff)
+    #expect(data[quadbyte: 1] == 0xE1000000)   // end of list = true, lowercase 'a', isWord
+  }
+
+  @Test
+  func convert_multiple_words_to_buffer() {
+    let words = ["BE", "BED", "BET", "AS"]
+    let trie = TrieBuilder().add(words).root
+    let sut = DataBuilder()
+
+    let data = sut.make(trie: trie)
+
+    #expect(data[quadbyte: 0] == 0x00ffffff)
+    #expect(data[quadbyte: 1] == 0x4200000C)    // 'B'
+    #expect(data[quadbyte: 2] == 0xC1000018)    // 'A', last match
+
+    #expect(data[quadbyte: 3] == 0xE5000010)    // 'e', last match
+
+    #expect(data[quadbyte: 4] == 0x64000000)    // 'd'
+    #expect(data[quadbyte: 5] == 0xF4000000)    // 't', last match
+
+    #expect(data[quadbyte: 6] == 0xF3000000)    // 's', last match
+  }
+}
