37aa2d943d9380a7e76274547ec3e4729af5b715
Bill Wake <william.wake@acm.org>
2026-06-16 09:59:53 -0400

    Make test sort words

--- betwist/TrieGeneratorTests/ADataBuilder.swift
+++ betwist/TrieGeneratorTests/ADataBuilder.swift
@@ -15,20 +15,21 @@ struct ADataBuilder {
 
   @Test
   func convert_multiple_words_to_buffer() {
-    let words = ["BE", "BED", "BET", "AS"]
+    let words = ["BE", "BED", "BET", "AS"].sorted()
     let trie = TrieBuilder().add(words).root
     let sut = DataBuilder()
 
     let data = sut.make(trie: trie)
 
-    #expect(data[matchEntry: 0] == 0x42000002)    // 'B'
-    #expect(data[matchEntry: 1] == 0xC1000005)    // 'A', last match
+    #expect(data[matchEntry: 0] == 0x41000002)    // 'A'
+    #expect(data[matchEntry: 1] == 0xC2000003)    // 'B', last match
 
-    #expect(data[matchEntry: 2] == 0xE5000003)    // 'e', last match
+    #expect(data[matchEntry: 2] == 0xF3000000)    // 's', last match
 
-    #expect(data[matchEntry: 3] == 0x64000000)    // 'd'
-    #expect(data[matchEntry: 4] == 0xF4000000)    // 't', last match
+    #expect(data[matchEntry: 3] == 0xE5000004)    // 'e', last match
 
-    #expect(data[matchEntry: 5] == 0xF3000000)    // 's', last match
+    #expect(data[matchEntry: 4] == 0x64000000)    // 'd'
+    #expect(data[matchEntry: 5] == 0xF4000000)    // 't', last match
+
   }
 }
