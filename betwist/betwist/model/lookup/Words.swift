205dcc749690c22218667c74ca358bda0471e3e7
Bill Wake <william.wake@acm.org>
2026-06-30 22:30:10 -0400

    More public structs

--- betwist/betwist/model/lookup/Words.swift
+++ betwist/betwist/model/lookup/Words.swift
@@ -1,19 +1,19 @@
 import Foundation
 
-struct Words: Decodable {
+public struct Words: Decodable {
   let entries: [WordEntry]
 }
 
-struct WordEntry: Decodable, Equatable, Hashable {
+public struct WordEntry: Decodable, Equatable, Hashable {
   let word: String
 //  let meanings: [Meaning]
 }
 
-struct Meaning: Decodable, Equatable, Hashable {
+public struct Meaning: Decodable, Equatable, Hashable {
   let partOfSpeech: String
   let definitions: [Definition]
 }
 
-struct Definition: Decodable, Equatable, Hashable {
+public struct Definition: Decodable, Equatable, Hashable {
   let definition: String
 }
