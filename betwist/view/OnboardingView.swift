b396fe81ef1fccc0332f11c92a48c8eeceb97b15
Bill Wake <william.wake@acm.org>
2026-07-21 12:15:15 -0400

    Add onboarding images

--- betwist/view/OnboardingView.swift
+++ betwist/view/OnboardingView.swift
@@ -6,23 +6,53 @@ struct OnboardingView: View {
 
   var body: some View {
     TabView {
-      Text("A")
-        .padding()
-        .background(.yellow)
-        .frame(maxWidth: .infinity)
-      Text("B")
-        .backgroundStyle(.yellow)
-      Color(.green)
+      Image(.onboardTitle)
+        .onboard()
+        .accessibilityLabel("Betwist - Finding words with a twist. The letters are in a repeating grid")
+
+      Image(.onboardFormWord)
+        .onboard()
+        .accessibilityLabel("Tap neighbors to build a word")
+
+      Image(.onboardUndo)
+        .onboard()
+        .accessibilityLabel("Tap an earlier letter to undo")
+
+      Image(.onboardScore)
+        .onboard()
+        .accessibilityLabel("Tap the last letter twice to score. You get credit for shorter words too!")
+
+      Image(.onboardTwist)
+        .onboard()
+        .accessibilityLabel("Tap a twist button if you get stuck")
+
+      Image(.onboardReveal)
+        .onboard()
+        .accessibilityLabel("'Reveal' shows words you and the system found")
+
+      Image(.onboardDefinition)
+        .onboard()
+        .accessibilityLabel("Tap the magnifying glass to see a definition (if we have it)")
     }
-    .backgroundStyle(.yellow)
     .indexViewStyle(.page(backgroundDisplayMode: .always))
     .tabViewStyle(.page)
     .overlay(
       Button("Close", systemImage: "xmark") { dismiss() }
         .labelStyle(.iconOnly)
-        .offset(x: -50, y: 50),
+        .offset(x: -15, y: 15)
+        .foregroundStyle(.black),
       alignment: .topTrailing
     )
+  }
+}
+
+extension Image {
+  func onboard() -> some View {
+    self
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+      .clipped()
+      .padding()
   }
 }
 
