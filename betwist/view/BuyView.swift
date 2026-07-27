bd082dc253a547dc5cf0ce56f838b4cfe2c990d1
Bill Wake <william.wake@acm.org>
2026-07-27 11:40:18 -0400

    Simple Buy View

--- betwist/view/BuyView.swift
+++ betwist/view/BuyView.swift
@@ -1,8 +1,23 @@
 import SwiftUI
 
 struct BuyView: View {
+  @Environment(\.dismiss)
+  private var dismiss
+
   var body: some View {
-    Text( "Buy World!" )
+    Text("Get More Games")
+      .font(.title)
+
+    Text("You've played 20 free games - with no ads!")
+    Text("You can purchase this game with a one-time payment: $4.99.")
+    Text("You won't have ads then either.")
+
+    Spacer()
+
+    Button("Done") {
+      dismiss()
+    }
+    .capsuled()
   }
 }
 
