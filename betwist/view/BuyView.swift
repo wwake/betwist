acbf03553f05d50b850371c5456a19ad56b16bde
Bill Wake <william.wake@acm.org>
2026-07-27 12:03:01 -0400

    Set up StoreKit for testing

--- betwist/view/BuyView.swift
+++ betwist/view/BuyView.swift
@@ -1,16 +1,31 @@
+import StoreKit
 import SwiftUI
 
 struct BuyView: View {
   @Environment(\.dismiss)
   private var dismiss
 
+  //  @Environment(Store.self) private var store: Store
+
   var body: some View {
+  //  @Bindable var store = store
+
     Text("Get More Games")
       .font(.title)
 
     Text("You've played 20 free games - with no ads!")
     Text("You can purchase this game with a one-time payment: $4.99.")
     Text("You won't have ads then either.")
+
+    Spacer()
+
+//    VStack {
+//      // ProductID.all is an array of your product ID strings.
+//      StoreView(ids: ProductID.all)
+//        .storeButton(.hidden, for: .cancellation)
+//        .storeButton(.visible, for: .restorePurchases)
+//    }
+//    .padding()
 
     Spacer()
 
